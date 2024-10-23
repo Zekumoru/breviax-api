99881d2b10f521ec451e1938cf98dafad12d2573
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-10-23 23:22:09 +0200

    Fix tmp uploads folder not created

--- middlewares/upload.ts
+++ middlewares/upload.ts
@@ -1,9 +1,14 @@
 import multer from 'multer';
 import appConfig from '../appConfig';
+import fs from 'fs';
 
 const upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
+      if (!fs.existsSync(appConfig.paths.UPLOAD_FOLDER)) {
+        fs.mkdirSync(appConfig.paths.UPLOAD_FOLDER, { recursive: true });
+      }
+
       cb(null, appConfig.paths.UPLOAD_FOLDER);
     },
     filename: (req, file, cb) => {
