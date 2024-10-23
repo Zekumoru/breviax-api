af82af39df610c281649068e22f34845c20b203a
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-10-23 12:51:07 +0200

    Add deepgram api key

--- types/global/env.d.ts
+++ types/global/env.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       NODE_ENV?: 'development' | 'production';
       SUPPORT_LARGE_MODEL?: string;
+      DEEPGRAM_API_KEY?: String;
       PORT?: string;
       HUGGING_FACE_TOKEN?: string;
       OPENAI_API_KEY?: string;
